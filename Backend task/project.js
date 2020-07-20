0e8069944c7e74b15986babc088a9cf8b2f8a193
kamel <mohamed.kamel.m73@gmail.com>
2020-07-20 02:12:28 +0200

    first commit

---
+++ Backend task/project.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var http = require('http');
+var fs = require ('fs');
+
+
+http.createServer(function (req,res){
+    fs.readFile('proj.html', function(err,data){
+        res.writeHead(200,{'Content-Type':'text/html'});
+        res.write(data);
+        return res.end();
+        
+    });
+
+
+}).listen(8080);
+
+console.log('server running');
